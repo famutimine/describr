8954d638ce7efe95ac91f9c6ed4e614ad437cac7
GitHub Action <action@github.com>
2024-02-07 04:34:39 +0000

    Bump version: 0.0.30 → 0.0.31

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with urlopen("https://raw.githubusercontent.com/famutimine/describr/main/README.
 
 setuptools.setup(
     name='describr',
-    version='0.0.30',
+    version='0.0.31',
     description='Describr is a Python library that provides a convenient way to generate descriptive statistics for datasets.',
     long_description=long_description,
     long_description_content_type='text/markdown',
